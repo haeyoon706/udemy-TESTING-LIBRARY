3fa67dd257d42da27b910ef7b3da961eb7bb41bb
haeyoon706 <haeyoon706@gmail.com>
2026-02-20 09:16:15 +0900

    Section2

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,8 +1,84 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+const { kebabCaseToTitleCase } = require("./helpers");
 
-test("App contains correct heading", () => {
+test("button click flow", () => {
+  // render App
   render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument();
+
+  // find the button
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+
+  // check initial color
+  expect(buttonElement).toHaveClass("medium-violet-red");
+
+  // click the button
+  fireEvent.click(buttonElement);
+
+  // check button text
+  expect(buttonElement).toHaveTextContent(/red/i);
+
+  // check the button color
+  expect(buttonElement).toHaveClass("midnight-blue");
+});
+
+test("checkbox flow", () => {
+  render(<App />);
+
+  // find elements
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+  const checkboxElement = screen.getByRole("checkbox", {
+    name: /disable button/i,
+  });
+
+  // check initial conditions
+  expect(buttonElement).toBeEnabled();
+  expect(checkboxElement).not.toBeChecked();
+
+  // click checkbox to disable button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeDisabled();
+  expect(buttonElement).toHaveClass("gray");
+
+  // click checkbox again to re-enable button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeEnabled();
+  expect(buttonElement).toHaveClass("medium-violet-red");
+});
+
+test("checkbox flow after button click", () => {
+  render(<App />);
+
+  // find elements
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+  const checkboxElement = screen.getByRole("checkbox", {
+    name: /disable button/i,
+  });
+
+  // check button to change to blue
+  fireEvent.click(buttonElement);
+
+  // click checkbox to disable button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeDisabled();
+  expect(buttonElement).toHaveClass("gray");
+
+  // click checkbox again to re-enable button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeEnabled();
+  expect(buttonElement).toHaveClass("midnight-blue");
+});
+
+describe("kebabCaseToTitleCase", () => {
+  test("workds for no hyphens", () => {
+    expect(kebabCaseToTitleCase("red")).toBe("Red");
+  });
+
+  test("workds for one hyphen", () => {
+    expect(kebabCaseToTitleCase("midnight-blue")).toBe("Midnight Blue");
+  });
+
+  test("workds for multiple hyphens", () => {
+    expect(kebabCaseToTitleCase("medium-violet-red")).toBe("Medium Violet Red");
+  });
 });
