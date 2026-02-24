bb372efdfe47caa0959dc8a7702dc627c1a68c94
haeyoon706 <haeyoon706@gmail.com>
2026-02-24 12:02:39 +0900

    Section8

--- sundae-starter/src/pages/confirmation/OrderConfirmation.jsx
+++ sundae-starter/src/pages/confirmation/OrderConfirmation.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Button from "react-bootstrap/Button";
 import { useOrderDetails } from "../../contexts/OrderDetails";
+import AlertBanner from "../common/AlertBanner";
 
 export default function OrderConfirmation({ setOrderPhase }) {
   const { resetOrder } = useOrderDetails();
   const [orderNumber, setOrderNumber] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
@@ -15,9 +17,7 @@ export default function OrderConfirmation({ setOrderPhase }) {
       .then((response) => {
         setOrderNumber(response.data.orderNumber);
       })
-      .catch(() => {
-        // TODO: handle error
-      });
+      .catch(() => setError(true));
   }, []);
 
   function handleClick() {
@@ -31,6 +31,15 @@ export default function OrderConfirmation({ setOrderPhase }) {
   const newOrderButton = (
     <Button onClick={handleClick}>Create new order</Button>
   );
+
+  if (error) {
+    return (
+      <>
+        <AlertBanner message={null} variant={null} />
+        {newOrderButton}
+      </>
+    );
+  }
 
   if (orderNumber) {
     return (
